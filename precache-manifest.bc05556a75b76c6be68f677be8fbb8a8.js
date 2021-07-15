55592865ad90f616e736f0b2783767f025fcc1f0
tangtao <tangtao2099@outlook.com>
2021-07-15 09:50:30 +0800

    opt: update

---
+++ precache-manifest.bc05556a75b76c6be68f677be8fbb8a8.js
@@ -0,0 +1,45 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "url": "https://tangtaoshadow.github.io/fiber-studio/54b5f2f6b82f420a87ddb8da3b8109ad.svg"
+  },
+  {
+    "revision": "3ab4fb0901ad6a35e0a7",
+    "url": "https://tangtaoshadow.github.io/fiber-studio/css/app.b0aefef5.css"
+  },
+  {
+    "revision": "b3df5a75d3741f19d5b2",
+    "url": "https://tangtaoshadow.github.io/fiber-studio/css/vendors-app.27cab968.css"
+  },
+  {
+    "revision": "03341ca0b0760d11ca288e71f85f4727",
+    "url": "https://tangtaoshadow.github.io/fiber-studio/index.html"
+  },
+  {
+    "revision": "169ed76e05f10a840ad4",
+    "url": "https://tangtaoshadow.github.io/fiber-studio/js/about.e385b771.js"
+  },
+  {
+    "revision": "3ab4fb0901ad6a35e0a7",
+    "url": "https://tangtaoshadow.github.io/fiber-studio/js/app.1f1b0c6b.js"
+  },
+  {
+    "revision": "80b5f34e53aee3fde012",
+    "url": "https://tangtaoshadow.github.io/fiber-studio/js/chunk.17ff2af2bcc85.42da114c.js"
+  },
+  {
+    "revision": "0e9a99cd5559319885ac",
+    "url": "https://tangtaoshadow.github.io/fiber-studio/js/chunk.912715af1c4aa.70896510.js"
+  },
+  {
+    "revision": "b3df5a75d3741f19d5b2",
+    "url": "https://tangtaoshadow.github.io/fiber-studio/js/vendors-app.e54c47af.js"
+  },
+  {
+    "revision": "9fd3d612b912115f1f1c88e3e46703e7",
+    "url": "https://tangtaoshadow.github.io/fiber-studio/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "https://tangtaoshadow.github.io/fiber-studio/robots.txt"
+  }
+]);
