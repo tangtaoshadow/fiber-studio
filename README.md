3398c6401ebb20ab5bfa661211020003f08c1d14
tangtao <tangtao2099@outlook.com>
2021-07-15 09:48:32 +0800

    opt: deploy success 🐱‍

---
+++ README.md
@@ -0,0 +1,7 @@
+
+
+# fiber-studio 🦅
+> `createTime`: 20210715094640
+>
+> 链接：[Github Page 👁️](https://tangtaoshadow.github.io/fiber-studio/)
+
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.338d93000a249fdec9c53cafb18dd1c9.js"
+  "https://tangtaoshadow.github.io/fiber-studio/precache-manifest.bc05556a75b76c6be68f677be8fbb8a8.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "fiber-visualization"});
